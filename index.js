4f4a495aef94b182da4a32118f89039556dd17dc
Cole Willsea <coleww@gmail.com>
2015-07-12 09:12:58 -0700

    building oscillators

---
+++ index.js
@@ -0,0 +1,85 @@
+var typey = require('get-object-type')
+
+module.exports = function(node){
+  var el = document.createElement('div')
+  switch (typey(node)){
+    case 'OscillatorNode':
+      createOscillator(el, node)
+      break
+    // case 'DelayNode':
+
+    //   break
+    // case 'GainNode':
+
+    //   break
+    // case 'ConvolverNode':
+
+    //   break
+    // case 'WaveShaperNode':
+
+    //   break
+    // case 'AudioBufferSourceNode':
+
+    //   break
+    // case 'PannerNode':
+
+    //   break
+    // case 'BiquadFilterNode':
+
+    //   break
+    default:
+      console.log('bork')
+  }
+  return el
+}
+
+function createOscillator(el, node){
+  el.setAttribute("class", "oscillator")
+
+  var freqRange = document.createElement("input")
+  freqRange.setAttribute("type", "range")
+  freqRange.setAttribute("class", "freq-range")
+  freqRange.setAttribute("min", 1)
+  freqRange.setAttribute("value", node.frequency.value)
+  freqRange.setAttribute("max", 20000)
+  var freqInfo = document.createElement("span")
+  freqInfo.setAttribute("class", "freq-info")
+  freqInfo.textContent = node.frequency.value
+  freqRange.addEventListener("change", function(e){
+    node.frequency.value = e.target.valueAsNumber
+    freqInfo.textContent = e.target.valueAsNumber
+  })
+  el.appendChild(freqInfo)
+  el.appendChild(freqRange)
+
+  var detuneRange = document.createElement("input")
+  detuneRange.setAttribute("type", "range")
+  detuneRange.setAttribute("class", "detune-range")
+  detuneRange.setAttribute("min", -2000)
+  console.log(node.detune.value)
+  detuneRange.setAttribute("value", node.detune.value)
+  detuneRange.setAttribute("max", 2000)
+  var detuneInfo = document.createElement("span")
+  detuneInfo.setAttribute("class", "detune-info")
+  detuneInfo.textContent = node.detune.value
+  detuneRange.addEventListener("change", function(e){
+    node.detune.value = e.target.valueAsNumber
+    detuneInfo.textContent = e.target.valueAsNumber
+  })
+  el.appendChild(detuneInfo)
+  el.appendChild(detuneRange)
+
+  var typeSelect = document.createElement("select")
+  typeSelect.setAttribute("type", "text")
+  typeSelect.setAttribute("class", "type-select")
+  var types = ['sine', 'square', 'sawtooth', 'triangle']
+  types.forEach(function(type){
+    var opt = document.createElement("option")
+    opt.value = opt.textContent = type
+    typeSelect.appendChild(opt)
+  })
+  typeSelect.addEventListener("change", function(e){
+    node.type = e.target.value
+  })
+  el.appendChild(typeSelect)
+}
--- test.js
+++ test.js
@@ -1,4 +1,71 @@
 var tap = require('tape')
-tap.test('',function(t){
+var triggerChange = require('trigger-change')
 
+var context = new (window.AudioContext || window.webkitAudioContext)()
+var webAudioUI = require('./')
+
+tap.test('Oscillator',function(t){
+  t.plan(6)
+
+  var osc = context.createOscillator()
+  var el = webAudioUI(osc)
+  console.log(osc.detune.value)
+  var typeSelect = el.querySelector('.type-select')
+  t.looseEqual(typeSelect.value, 'sine', 'default')
+  triggerChange(typeSelect, 'sawtooth')
+  t.looseEqual(osc.type, 'sawtooth', 'updates osc')
+
+  var freqRange = el.querySelector('.freq-range')
+  t.looseEqual(freqRange.value, 440, 'default')
+  triggerChange(freqRange, 1000)
+  t.looseEqual(osc.frequency.value, 1000, 'updates osc')
+
+  var detuneRange = el.querySelector('.detune-range')
+  t.looseEqual(detuneRange.value, 0, 'default')
+  triggerChange(detuneRange, 400)
+  t.looseEqual(osc.detune.value, 400, 'updates osc')
+  triggerChange(detuneRange, 0)
+  document.body.appendChild(el)
 })
+
+// tap.test('BiquadFilter',function(t){
+//   var osc = context.createBiquadFilter()
+//   var el = webAudioUI(osc)
+
+// })
+
+// tap.test('Gain',function(t){
+//   var osc = context.createGain()
+//   var el = webAudioUI(osc)
+
+// })
+
+// tap.test('Convolver',function(t){
+//   var osc = context.createConvolver()
+//   var el = webAudioUI(osc)
+
+// })
+
+// tap.test('WaveShaper',function(t){
+//   var osc = context.createWaveShaper()
+//   var el = webAudioUI(osc)
+
+// })
+
+// tap.test('AudioBufferSource',function(t){
+//   var osc = context.createAudioBufferSource()
+//   var el = webAudioUI(osc)
+
+// })
+
+// tap.test('Delay',function(t){
+//   var osc = context.createDelay()
+//   var el = webAudioUI(osc)
+
+// })
+
+// tap.test('Panner',function(t){
+//   var osc = context.createPanner()
+//   var el = webAudioUI(osc)
+
+// })
