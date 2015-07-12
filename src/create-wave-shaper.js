b4fdea9c2f792bbbd34bc0d6edb980c47e542e51
Cole Willsea <coleww@gmail.com>
2015-07-12 11:35:47 -0700

    a

--- src/create-wave-shaper.js
+++ src/create-wave-shaper.js
@@ -1,4 +1,8 @@
+var makeDistortionCurve = require('make-distortion-curve')
+
 module.exports = function(el, node){
+  var amount = 100
+  node.curve = makeDistortionCurve(100)
   el.setAttribute("class", "node-container gain")
 
   var label = document.createElement("span")
@@ -6,6 +10,28 @@ module.exports = function(el, node){
   label.setAttribute("class", "label node-label")
   el.appendChild(label)
 
+  var amountContainer = document.createElement("div")
+  amountContainer.setAttribute("class", "amount-container el-container")
+  var amountRange = document.createElement("input")
+  amountRange.setAttribute("type", "range")
+  amountRange.setAttribute("class", "amount-range")
+  amountRange.setAttribute("min", 0)
+  amountRange.setAttribute("value", amount)
+  amountRange.setAttribute("max", 1000)
+  var amountInfo = document.createElement("span")
+  amountInfo.setAttribute("class", "amount-info")
+  amountInfo.textContent = amount
+  amountRange.addEventListener("change", function(e){
+    node.curve = makeDistortionCurve(e.target.valueAsNumber)
+    amountInfo.textContent = e.target.valueAsNumber
+  })
+  var amountLabel = document.createElement("span")
+  amountLabel.textContent = "amount"
+  amountLabel.setAttribute("class", "label el-label")
+  amountContainer.appendChild(amountLabel)
+  amountContainer.appendChild(amountRange)
+  amountContainer.appendChild(amountInfo)
+  el.appendChild(amountContainer)
 
   var overSampleContainer = document.createElement("div")
   overSampleContainer.setAttribute("class", "over-sample-container el-container")
