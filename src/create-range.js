45e15d16c4b56eb5e2edcc19d74a61541bc53e46
Cole Willsea <coleww@gmail.com>
2018-10-20 18:09:23 -0700

    set range label value on setup, format files better :D

--- src/create-range.js
+++ src/create-range.js
@@ -1,6 +1,7 @@
 module.exports = function createRange(config) {
   var container = document.createElement("div")
   container.setAttribute("class", config.attribute + "-container el-container")
+
   var rangeInput = document.createElement("input")
   rangeInput.setAttribute("type", "range")
   rangeInput.setAttribute("class", config.attribute + "-range")
@@ -8,15 +9,19 @@ module.exports = function createRange(config) {
   rangeInput.setAttribute("max", config.max)
   rangeInput.setAttribute("step", config.step)
   rangeInput.setAttribute("value", config.value)
+
   var rangeLabel = document.createElement("span")
   rangeLabel.setAttribute("class", config.attribute + "-info")
+  rangeLabel.textContent = config.value
   rangeInput.addEventListener("change", function(e){
     config.update(e.target.valueAsNumber)
-    rangeLabel.textContent = e.target.valueAsNumber
+    rangeLabel.textContent = e.target.valueAsNumber.toFixed(3);
   })
+
   var freqLabel = document.createElement("span")
   freqLabel.textContent = config.label
   freqLabel.setAttribute("class", config.attribute + "-label label el-label")
+
   container.appendChild(freqLabel)
   container.appendChild(rangeInput)
   container.appendChild(rangeLabel)
--- src/create-select.js
+++ src/create-select.js
@@ -1,6 +1,7 @@
 module.exports = function createSelect(config) {
   var selectContainer = document.createElement("div")
   selectContainer.setAttribute("class", config.attribute + "-container el-container")
+
   var selectInput = document.createElement("select")
   selectInput.setAttribute("type", "text")
   selectInput.setAttribute("class", config.attribute + "-select")
@@ -13,10 +14,13 @@ module.exports = function createSelect(config) {
   selectInput.addEventListener("change", function(e){
     config.update(e.target.value)
   })
+
   var selectLabel = document.createElement("span")
   selectLabel.textContent = "type"
   selectLabel.setAttribute("class", config.attribute + "-label label el-label")
+
   selectContainer.appendChild(selectLabel)
   selectContainer.appendChild(selectInput)
+  
   return selectContainer
 }
