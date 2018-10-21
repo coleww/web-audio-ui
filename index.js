6711177120c39229cb4d51d8e534189a21474ed5
Cole Willsea <coleww@gmail.com>
2018-10-20 17:40:42 -0700

    cleanup, add nullcheck for default config

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ var defaultConfigs = require('./src/config')
 module.exports = function(node, cfg){
   var config = cfg || {}
   var type = config.type || typey(node)
-  console.log(defaultConfigs, type)
-  var defaultConfig = defaultConfigs[type](node)
-
-  // my brain tells me i should add nullchecks here, 
-  // but my heart says that this works and is more elegant
-  // bite me
+  var defaultConfig = defaultConfigs[type] && defaultConfigs[type](node)
   var mergedConfig = Object.assign({}, defaultConfig, config)
-
   return createEl(node, mergedConfig)
 }
